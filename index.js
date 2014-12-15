a22827f05d01c29ded96a593ebf1ce99ed357817
leviross <levross@gmail.com>
2014-12-15 13:55:59 -0800

    Added new views, finished authentication

--- index.js
+++ index.js
@@ -1,51 +1,144 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var session = require('express-session');
+var express = require("express");
+var bodyParser = require("body-parser");
+var session = require("express-session");
 var app = express();
-var bcrypt = require('bcrypt');
-var db = require('./models/index.js');
-var request = require('request');
+var bcrypt = require("bcrypt");
+var db = require("./models/index.js");
+var request = require("request");
+var flash = require("connect-flash");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(session({
-    secret: "something",
-    resave: false,
-    saveUninitialized: true
+	secret: "something",
+	resave: false,
+	saveUninitialized: true
 }));
 
-// app.use(function (req, res, next) {
-//     req.getUser() = function() {
-//         return req.session.user || false;
-//     }
-//     next();
-// });
+app.use(flash());
+app.use(function (req, res, next) {
+    req.getUser = function() {
+        return req.session.user || false;
+    }
+    next();
+});
 
 
 
 app.get("/", function (req, res) {
-    res.render('homepage');
+	res.render("homepage");
+});
+app.get("*", function (req, res, next) {
+	var alerts = req.flash();
+	res.locals.alerts = alerts; 
+	next();
 });
 
 app.get("/search", function (req, res) {
 	var countryName = req.query.country;
 	request("http://restcountries.eu/rest/v1/name/" + countryName, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-			JSONObject results = new JSONObject();
-			var oldArray = req.body;
-			results.put("newArray", oldArray);
-			
-			res.send(results);
-			// res.render("search", {test: body});
+			var stuff = JSON.parse(body);
+			if(Array.isArray(stuff) && stuff.length > 0) {
+				res.render("search", {test: stuff});	
+			}else {
+
+			//TODO: MAKE THIS INTO RES RENDER LATER
+				res.send("some error message that i need to work on")
+			}
+		}else {
+			res.render("countryNotFound");
 		}
-	})
+	
+	});
+});
+app.get("/show/:id", function (req, res) {
+	var countryName = req.params.id;
+	request("http://restcountries.eu/rest/v1/name/" + countryName, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var stuff2 = JSON.parse(body);
+			if(Array.isArray(stuff2) && stuff2.length > 0) {
+				res.render("show", stuff2[0]);
+			}else {
+				//TODO: MAKE THIS INTO RES RENDER LATER
+				res.send("we found no country by that name")
+			}
+		}else {
+			res.render("countryNotFound");
+		}
+	});
+
 });
 
 
 
+app.get("/signup", function (req, res) {
+	res.render("signup");
+});
 
+app.get("/login", function (req, res) {
+	res.render("login");
+});
+
+app.post("/signup", function (req, res) {
+	db.user.findOrCreate(
+	{
+		where: {email: req.body.email},
+		defaults: {email: req.body.email, name: req.body.name, password: req.body.password}
+	}).spread(function (user, created) {
+		if (created) {
+			req.flash("info", "Welcome to My Travel App!");
+			res.redirect("/");
+		} else {
+			req.flash("warning", "You have already signed up for an account, please login.");
+			res.redirect("/login");
+		}
+
+	}).catch(function (error) {
+		if (error && Array.isArray(error.errors)) {
+			error.errors.forEach(function (errorItem) {
+				req.flash("danger", errorItem.message);
+			});
+		} else {
+			req.flash("danger", "Unknown Error");
+		}
+		res.redirect("/signup");
+	});
+});
+
+app.post("/login", function (req, res) {
+	db.user.find({where: {email: req.body.email}}).then(function (userObj) {
+		if (userObj) {
+			bcrypt.compare(req.body.password, userObj.password, function (err, match) {
+				if(match === true) {
+					//store user object in session
+					req.session.user = {
+						name: userObj.name,
+						id: userObj.id,
+						email: userObj.email
+					};
+					res.redirect("/");
+				}else {
+					req.flash("danger", "Please enter your correct password.");
+					res.redirect("/login");
+				}
+			})	
+		} else {
+			req.flash("warning", "Please enter a correct user email.");
+			res.redirect("/login");
+		}		
+
+
+	});
+});
+
+app.get("/logout", function (req, res) {
+	delete req.session.user;
+	req.flash("info", "You have been logged out.");
+	res.redirect("/");
+});
 
 
 
